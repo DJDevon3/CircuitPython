627e979873f33fab84a5e15de7c9c258a24d77e7
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2022-06-22 02:31:20 -0400

    Set to post temperature value as default
    Since the demo is for a feather sense we'll use the onboard temp sensor to publish a real value.

--- Airlift AdafruitIO Test/code.py
+++ Airlift AdafruitIO Test/code.py
@@ -3,6 +3,8 @@ import board
 import busio
 import displayio
 from digitalio import DigitalInOut
+import adafruit_bmp280
+import adafruit_sht31d
 import neopixel
 from adafruit_esp32spi import adafruit_esp32spi
 from adafruit_esp32spi import adafruit_esp32spi_wifimanager
@@ -12,6 +14,11 @@ displayio.release_displays()
 # This sketch should also work for the 2.5" TFT, just change the size.
 DISPLAY_WIDTH = 480
 DISPLAY_HEIGHT = 320
+
+i2c = board.I2C()
+bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+sht31d = adafruit_sht31d.SHT31D(i2c)
+bmp280.sea_level_pressure = bmp280.pressure
 
 # Get wifi details and more from a secrets.py file
 try:
@@ -37,31 +44,38 @@ tft_dc = board.D10
 display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs)
 display = HX8357(display_bus, width=DISPLAY_WIDTH, height=DISPLAY_HEIGHT)
 
-counter = 0
-print("Airlift Featherwing ESP32 SPI AdafruitIO Test\n")
+# Connect to WiFi
+print("Connecting to WiFi...")
+wifi.connect()
+print("Connected!\n")
 
+# Value to be posted to AdafruitIO (NRF52840 on-board temp sensor)
+update_val = "{:.1f}".format(bmp280.temperature*1.8+32)
+
+# Your feed key might be different from the feed name (no underscore allowed).
+# It's not your IO account key or feed name. It's the individual key for each feed.
+# For example, create a feed "temp_test" at AdafruitIO it sets the key "temp-test".
+feed_key = "sense-temp"
+    
 while True:
     try:
-        print("Posting data...", end="")
-        data = counter
-        feed = "test"
-        payload = {"value": data}
+        print("Value: %.1f" % float(update_val))
+        print("Posting Value...", end="")
+        payload = {"value": (update_val)}
         response = wifi.post(
             "https://io.adafruit.com/api/v2/"
             + secrets["aio_username"]
             + "/feeds/"
-            + feed
+            + feed_key
             + "/data",
             json=payload,
             headers={"X-AIO-KEY": secrets["aio_key"]},
         )
         print(response.json())
         response.close()
-        print("Count: {}".format(counter) + "\n")
-        counter = counter + 1
     except (ValueError, RuntimeError) as e:
         print("Failed to get data, retrying\n", e)
         wifi.reset()
         continue
     response = None
-    time.sleep(10)
+    time.sleep(3600)
