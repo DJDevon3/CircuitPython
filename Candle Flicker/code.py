4220735878a32bcc9142d6e0d49cde7afb671e86
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2022-07-12 03:46:10 -0400

    Retuned Breeze and Gust
    Breeze and Gust examples to be different enough to be noticeable with the naked eye.

--- Candle Flicker/code.py
+++ Candle Flicker/code.py
@@ -11,22 +11,33 @@ from analogio import AnalogOut
 analog_out = AnalogOut(board.A0)
 
 # Manually Set Flicker Type = Breeze or Gust
-Flicker_Type = ("Gust")
+Flicker_Type = ("Breeze")
 
-Breeze_Flicker = random.randint(50000, 60000)
-Gust_Flicker = random.randint(10000, 30000)
+Breeze_Low = random.randint(40000, 50000)
+Breeze_High = random.randint(50000, 60000)
+
+Gust_Low = random.randint(10000, 50000)
+Gust_High = random.randint(50000, 60000)
+
 random_low = random.randint(1000, 50000)
+random_high = random.randint(60000, 65535)
+rand_sleep = random.uniform(.005, 0.007)
 while True:
     if Flicker_Type == ("Breeze"):
-        Breeze_Flicker = random_low
+        Breeze_Low = random_low
+        Breeze_High = random_high
+        rand_sleep = random.uniform(.005, 0.007)
     elif Flicker_Type == ("Gust"):
-        Gust_Flicker = random_low
+        Gust_Low = random_low
+        Gust_High = random_high
+        rand_sleep = random.uniform(.01, 0.03)
     else:
         random_low
+        random_high
+        rand_sleep = random.uniform(.001, 0.003)
         pass
-    random.randint(50000, 60000)
-    random_high = random.randint(60000, 65535)
+    
+    # Random LED Flicker Low/High and Random Sleep Value
     analog_out.value = random.randint(random_low, random_high)
-    rand_sleep = random.uniform(.001, 0.005)
     time.sleep(rand_sleep)
     pass
